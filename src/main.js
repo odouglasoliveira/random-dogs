780a04ff6bd023f22c33ed73ba145dac6996428b
Douglas Oliveira <doglasolivera13@gmail.com>
2023-02-13 10:29:52 -0300

    Alterações no projeto e implementação do JS

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 import "./style.css";
+
+const button = document.querySelector('#dog-button');
+const imageDisplay = document.querySelector('.image-display')
+
+const generateRandomImage = (callback) => {
+  const URL = 'https://random.dog/woof.json';
+  return fetch(URL).then((response) => response.json()).then((data) => callback(data));
+};
+
+const insertImage = ({ url }) => {
+  const img = document.createElement('img');
+  img.src = url;
+  img.classList.add('image');
+  imageDisplay.innerHTML = '';
+  imageDisplay.appendChild(img);
+}
+
+button.addEventListener('click', () => {
+  generateRandomImage(insertImage)
+});
